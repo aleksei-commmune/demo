363fee9a15c2b559dee1bb9c77a08fe30045019e
aleksei-commmune <ivanov.aleksei@commmune.jp>
2023-03-02 14:21:20 +0900

    fix

--- e2e/split_test_files.sh
+++ e2e/split_test_files.sh
@@ -4,7 +4,7 @@ input_file=specs
 num_files=$1
 
 # List all spec files
-find e2e/cypress/e2e -type f -name "*.cy.js" > $input_file
+find cypress/e2e -type f -name "*.cy.js" > $input_file
 
 # Split specs into chunks
 total_lines=$(wc -l < $input_file)
@@ -16,4 +16,4 @@ if [ $remainder -gt 0 ]; then
 fi
 
 mkdir -p tmp
-split --lines=$lines_per_file $input_file "./tmp/spec_" --numeric-suffixes=1
+split --lines=$lines_per_file $input_file "../tmp/spec_" --numeric-suffixes=1
